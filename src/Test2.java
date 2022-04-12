3c449e4e4dd336a035a68e3f8c396316fdff265a
zjf <616616862@qq.com>
2022-04-12 19:22:18 +0800

    first commit

---
+++ src/Test2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Test2 {
+    public static void main(String[] args) {
+        Queue<Integer> list=new ArrayDeque<>();
+
+    }
+    public static boolean isValid(String s) {
+        Deque<Character> stack=new ArrayDeque();
+        for(char c:s.toCharArray()){
+            if(c=='('||c=='['||c=='{') stack.push(c);
+            else if(c==')'){if(stack.isEmpty()||stack.pop()!='(') return false;}
+            else if(c==']'){if(stack.isEmpty()||stack.pop()!='[') return false;}
+            else if(c=='}'){if(stack.isEmpty()||stack.pop()!='{') return false;}
+        }
+        return stack.isEmpty();
+    }
+}
